fb887a0419e09d0f7d0886a7b7b276d3437f59b4
Pedro Barbosa <135766848+PedroBDev@users.noreply.github.com>
2025-07-28 21:17:45 -0300

    V2

--- utils.py
+++
@@ -1,9 +0,0 @@
-def parse_input_tipo_conta():
-    while True:
-        tipo_conta = input('Informe o tipo de conta: [P] - Poupança / [C] - Corrente: ').strip().upper()
-        if tipo_conta == 'C':
-            return 'Corrente'
-        elif tipo_conta == 'P':
-            return 'Poupança'
-        else:
-            print("Entrada inválida. Por favor, digite 'P' para Poupança ou 'C' para Corrente.")
